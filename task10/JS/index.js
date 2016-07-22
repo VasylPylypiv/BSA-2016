dac496ddbcf4e9804753ed25811fcb000766a7fd
Vasyl Pylypiv <strange.viking@gmail.com>
2016-07-22 18:51:40 +0300

    Task #10

---
+++ task10/JS/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var server = app.listen(8080);
+console.log('Server is working on 8080 port');
+
+
+var staticDir = __dirname + '/public/';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+var messages = [];
+
+app.get('/', function(req, res){
+    res.sendFile(staticDir + 'index.html');
+
+});
+
+app.get('/messages', function(req,res){
+   res.json(messages);
+});
+
+app.post('/messages', function(req, res){
+    var message = req.body;
+    messages.push(message);
+    res.json(message);
+});
+
+/* io.on('connection', function(socket){
+
+    console.log('client connected');
+    socket.on('disconnected', function(msg){
+        console.log('client disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        messages.push(msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.emit('chat history', messages);
+}); */
